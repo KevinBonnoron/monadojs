fac21cdb747cb114e1b76d3455540fa4aaf38662
Kevin Bonnoron <2421321+KevinBonnoron@users.noreply.github.com>
2023-03-07 17:57:58 +0000

    feat: rollup commonjs + terser plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,30 +1,37 @@
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
+import typescript from 'rollup-plugin-typescript';
 import packageConfig from './package.json' assert { type: 'json' };
 
-const name = packageConfig.main.replace(/\.js$/, '');
+const name = packageConfig.main.replace(/\.cjs$/, '');
 
 const bundle = (config) => ({
   ...config,
   input: 'src/index.ts',
-  external: (id) => !/^[./]/.test(id),
 });
 
 export default [
   bundle({
-    plugins: [esbuild()],
+    plugins: [esbuild(), terser()],
     output: [
-      {
-        file: `${name}.js`,
-        format: 'cjs',
-        sourcemap: true,
-      },
       {
         file: `${name}.mjs`,
         format: 'es',
         sourcemap: true,
       },
     ],
+  }),
+  bundle({
+    plugins: [typescript(), commonjs(), terser()],
+    output: [
+      {
+        file: `${name}.cjs`,
+        format: 'commonjs',
+        sourcemap: true,
+      }
+    ]
   }),
   bundle({
     plugins: [dts()],
