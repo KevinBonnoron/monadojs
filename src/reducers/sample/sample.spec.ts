d5fc9565b0f7e069e3e188b77f4c674c8d976fe3
Kevin Bonnoron <2421321+KevinBonnoron@users.noreply.github.com>
2023-05-30 06:10:05 +0000

    fix: handling Math.random mock restore

--- src/reducers/sample/sample.spec.ts
+++ src/reducers/sample/sample.spec.ts
@@ -5,10 +5,8 @@ describe('sample', () => {
   it('should sample array', () => {
     const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    for (let i = 0; i < numbers.length; i++) {
-      vi.spyOn(global.Math, 'random').mockReturnValue(i / numbers.length);
-      expect(numbers.reduce(sample())).toStrictEqual(i);
-    }
-    vi.spyOn(global.Math, 'random').mockRestore();
+    const spy = vi.spyOn(global.Math, 'random').mockReturnValue(5 / numbers.length);
+    expect(numbers.reduce(sample())).toStrictEqual(5);
+    spy.mockRestore();
   });
 });
