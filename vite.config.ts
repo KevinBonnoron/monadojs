574c071598ebed1f9d271186db82dd09fef2b4e3
Kevin Bonnoron <2421321+KevinBonnoron@users.noreply.github.com>
2024-04-05 10:38:23 +0000

    feat(packaging): move rollup config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  build: {
+    minify: false,
+    target: 'modules',
+    outDir: './dist',
+    lib: {
+      entry: resolve(__dirname, './src/index.ts'),
+      formats: ['cjs', 'es'],
+      fileName: (format) => `monadojs.${format.replace('es', 'mjs')}`,
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
   test: {
-    passWithNoTests: true
-  }
+    passWithNoTests: true,
+  },
+  plugins: [dts({ rollupTypes: true })],
 });
